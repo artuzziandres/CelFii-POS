6ea0fa652d1678b8251f83bbd01b4252aed4f795
artuzziandres <artuzzi.andres@gmail.com>
2026-07-29 20:57:18 -0300

    Corregir imports del lector de códigos

--- app/src/main/java/com/celfii/pos/MainActivity.java
+++ app/src/main/java/com/celfii/pos/MainActivity.java
@@ -23,9 +23,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.mlkit.vision.codescanner.GmsBarcodeScanner;
-import com.google.android.gms.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
-import com.google.android.gms.mlkit.vision.codescanner.GmsBarcodeScanning;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
 import com.google.mlkit.vision.barcode.common.Barcode;
 
 import java.text.NumberFormat;
