08f533bb631ee3f11299c73f1946fe0385a9ba29
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 10:36:24 -0300

    Use stable debug signing identity

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
     }
 
     buildTypes {
