e6b5422c15556bf42f7885cb2da1796f3698085d
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 10:26:51 -0300

    Add sale details, thumbnails and scanner

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
@@ -25,4 +25,8 @@ android {
         "src/main/res",
         rootProject.file("app/src/main/res")
     )
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
 }
