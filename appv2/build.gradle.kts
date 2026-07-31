a156ef6b6b69f29bbb34bac293c3867a3bb78872
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 10:00:16 -0300

    Add stable Android v2 shell

---
+++ appv2/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.celfii.ventas"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.celfii.ventas"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
