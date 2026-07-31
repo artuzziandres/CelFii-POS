23c673408adf773b86d4c9d7023a453f32c8162d
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 11:24:32 -0300

    Versión 0.5.2 con impresión

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.5.1"
+        versionCode = 8
+        versionName = "0.5.2"
     }
 
     buildTypes {
