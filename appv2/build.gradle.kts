ba20b78e235128c6889a83b6c0f649c4526e6571
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 11:39:15 -0300

    Versión 0.5.4 con historial y cámara integrada

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.5.3"
+        versionCode = 10
+        versionName = "0.5.4"
     }
 
     buildTypes {
