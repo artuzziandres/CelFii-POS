3778a17eaca8e22586a9c183540823db3d7d5549
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 10:50:35 -0300

    Fix scanner photos seller and payment methods

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     buildTypes {
