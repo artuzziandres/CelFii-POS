52344055785361e1296584cec57d4ffeb207876a
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 18:38:36 -0300

    Versión 0.5.7 con conexión Bluetooth reforzada

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.5.6"
+        versionCode = 13
+        versionName = "0.5.7"
     }
 
     buildTypes {
