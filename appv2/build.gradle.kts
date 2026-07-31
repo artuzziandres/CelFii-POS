691351e08a40bcd238057f07003df7796382ab62
artuzziandres <artuzzi.andres@gmail.com>
2026-07-31 11:27:15 -0300

    Versión 0.5.3 con cierre de venta correcto

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.5.2"
+        versionCode = 9
+        versionName = "0.5.3"
     }
 
     buildTypes {
