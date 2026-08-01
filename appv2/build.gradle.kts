7e1b7e07c317f70821206f2764219fbcd03561a9
artuzziandres <artuzzi.andres@gmail.com>
2026-08-01 12:42:13 -0300

    Compilar Cel-Fii Ventas 0.6.3 con token actualizado

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.6.2"
+        versionCode = 18
+        versionName = "0.6.3"
         buildConfigField("String", "CELFII_API_URL", "\"https://script.google.com/macros/s/AKfycbwn9Btk_JoFU6KY-tPmQk2Ks7962bIYJuRLILJw-cj79lW-IZs2NDkku5xuK4xinjD_/exec\"")
         buildConfigField("String", "CELFII_API_TOKEN", "\"${apiToken.get()}\"")
     }
