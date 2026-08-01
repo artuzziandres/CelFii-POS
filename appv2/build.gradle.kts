925f339c5cb54410b2cd6841f13fc9f936d62175
artuzziandres <artuzzi.andres@gmail.com>
2026-08-01 12:12:26 -0300

    Corregir URL real del conector en 0.6.2

--- appv2/build.gradle.kts
+++ appv2/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins { id("com.android.application") }
 
-val apiUrl = providers.gradleProperty("CELFII_API_URL").orElse("https://script.google.com/macros/s/AKfycbwn9Btk_JoFU6KY-tPmQk2Ks7962bIYJuRLILJw-cj79lW-IZs2NDkku5xuK4xinjD_/exec")
 val apiToken = providers.gradleProperty("CELFII_API_TOKEN").orElse("")
 
 android {
@@ -11,10 +10,10 @@ android {
         applicationId = "com.celfii.ventas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.6.1"
-        buildConfigField("String", "CELFII_API_URL", "\"\${apiUrl.get()}\"")
-        buildConfigField("String", "CELFII_API_TOKEN", "\"\${apiToken.get()}\"")
+        versionCode = 17
+        versionName = "0.6.2"
+        buildConfigField("String", "CELFII_API_URL", "\"https://script.google.com/macros/s/AKfycbwn9Btk_JoFU6KY-tPmQk2Ks7962bIYJuRLILJw-cj79lW-IZs2NDkku5xuK4xinjD_/exec\"")
+        buildConfigField("String", "CELFII_API_TOKEN", "\"${apiToken.get()}\"")
     }
 
     buildFeatures { buildConfig = true }
